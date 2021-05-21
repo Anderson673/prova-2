925a20f25f65e0f6858060f50345e8e12e7a952e
AndersonSantos6 <>
2021-05-21 20:29:59 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int idade;
+  char nome;
+  cout << "por favor digite seu nome"<< endl;
+  cin >> nome;
+   cout << "por favor digite sua idade"<< endl;
+   cin >> idade;
+
+   
+  
+}
